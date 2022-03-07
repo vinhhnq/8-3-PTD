dac55d9323ce533af653daa218e22573097412af
quagvinhh <vinhng74@gmail.com>
2022-03-07 14:44:34 +0700

    update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = 'Pham Thuy Duong';
 const giftUrl = 'https://www.facebook.com/profile.php?id=100036321231452';
 const eventName = 'Chúc Mừng 8/3';
 const titleCard = 'Tặng người anh yêu';
-const contentCard = 'Chúc em 8/3 vui vẻ, luôn xinh đẹp trong mắt anh, luôn mỉm cười và hạnh phúc, chăm học để giỏi lên, hãy là 1 cô gái lập trình viên mạnh mẽ, anh yêu em rất nhiều <3';
+const contentCard = 'Chúc em 8/3 vui vẻ, luôn xinh đẹp trong mắt anh, luôn mỉm cười và hạnh phúc, chăm học để giỏi lên, hãy là 1 cô gái mạnh mẽ, anh yêu em rất nhiều <3';
 const giftImage = 'emyeu.png';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
